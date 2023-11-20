9890ab0e81f04870360f9ef8dc7b96e6c049d6b1
Daksh <115346924+Daksh7112003@users.noreply.github.com>
2023-11-20 16:41:13 +0530

    Create 0019_REMOVE_Nth_fromthelast.java

---
+++ 0019_REMOVE_Nth_fromthelast.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+    ListNode fast= head ;
+    ListNode slow = head ;
+     
+    while(n-->0) fast=fast.next;
+    while(fast != null && fast.next !=null ){
+        slow=slow.next;
+        fast= fast.next;
+
+    }
+
+    if(slow==null || slow.next==null) return null;
+    else if(slow==head && fast == null) head =head.next ;
+    
+
+   else  slow.next= slow.next.next;
+   return head;
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
