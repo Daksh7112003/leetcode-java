eabc0e0be25ff65c1c9a53fe3c0a5eca23e8fca0
Daksh <115346924+Daksh7112003@users.noreply.github.com>
2024-01-26 23:42:10 +0530

    0237_Delete_Node.java

--- 0237_Delete_Node.java
+++ 0237_Delete_Node.java
@@ -1,11 +1,3 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
 class Solution {
     public void deleteNode(ListNode node) {
 
