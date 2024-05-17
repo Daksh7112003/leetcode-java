984d669e368631e094da13c850f54c243e13f399
Daksh <115346924+Daksh7112003@users.noreply.github.com>
2024-05-18 02:11:42 +0530

    Create 0007_reverse_integer.cpp

---
+++ 0007_reverse_integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int ans = 0;
+        
+        while (x!= 0) {
+            int digit = x % 10;
+            
+            
+            if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && digit > 7)) {
+                return 0;
+            }
+            if (ans < INT_MIN / 10 || (ans == INT_MIN / 10 && digit < -8)) {
+                return 0;
+            }
+            
+            ans = ans * 10 + digit;
+            x /= 10;
+        }
+        
+        return ans;
+    }
+};
